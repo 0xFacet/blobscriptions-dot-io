6be37a8215d12c91d1eb4d54d591375c2a87c7d5
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-03-11 14:19:19 -0400

    Add fake signatures

--- src/connect.tsx
+++ src/connect.tsx
@@ -1,12 +1,11 @@
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
-import { BlobEIP4844Transaction } from '@ethereumjs/tx'
-import { Address, Kzg, bytesToHex, fromRpcSig, initKZG, randomBytes } from '@ethereumjs/util'
+import { BlobEIP4844Transaction, FeeMarketEIP1559Transaction } from '@ethereumjs/tx'
+import { Kzg, SECP256K1_ORDER_DIV_2, bytesToBigInt, bytesToHex, initKZG, randomBytes } from '@ethereumjs/util'
 import { useConnectWallet } from '@web3-onboard/react'
 import { createKZG } from 'kzg-wasm'
 import { useEffect, useState } from 'react'
 
 export default function ConnectButton() {
-  const [tx, setTx] = useState<BlobEIP4844Transaction>()
   const [kzg, setKzg] = useState<Kzg>()
   useEffect(() => {
     const init = async () => {
@@ -18,27 +17,49 @@ export default function ConnectButton() {
   }, [])
   const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
   const sendTx = async () => {
+
     const tip = await wallet!.provider.request({
       "method": "eth_feeHistory",
       "params": ['0x1', 'latest', [0.5]]
     }) as any
     console.log('tip', tip)
-    const nonce = await wallet!.provider.request({
-      "method":'eth_getTransactionCount',
-      "params":[wallet?.accounts[0].address]
-    }) as string
-    console.log(nonce)
     const common = new Common({ chain: Chain.Sepolia, hardfork: Hardfork.Cancun , customCrypto: { kzg }})
-    const tx = BlobEIP4844Transaction.fromTxData({ nonce: parseInt(nonce), blobsData: ['hello world'], to: '0xff00000000000000000000000000000000074248', gasLimit: 0x5208, maxFeePerGas: parseInt(tip.baseFeePerGas[0] + 5), maxFeePerBlobGas: 0xf, maxPriorityFeePerGas: parseInt(tip.reward[0])}, { common })
-    setTx(tx)
-    const res = await wallet!.provider.request({
-      "method": "eth_sign",
-      "params": [wallet?.accounts[0].address,bytesToHex(tx?.getHashedMessageToSign())]
+    const blobTx = BlobEIP4844Transaction.fromTxData({ nonce: 0x0, blobsData: ['hello world'], to: '0xff00000000000000000000000000000000074248', gasLimit: 0x5208, maxFeePerGas: parseInt(tip.baseFeePerGas[0] + 5), maxFeePerBlobGas: 0xf, maxPriorityFeePerGas: parseInt(tip.reward[0])}, { common })
+
+    const r = randomBytes(32)
+    let s = randomBytes(32)
+    while (bytesToBigInt(s) > SECP256K1_ORDER_DIV_2) {
+      s = randomBytes(32)
+    }
+    const v = 0n
+    const signedTx = blobTx?.addSignature(v, r, s)
+    console.log(signedTx.getSenderAddress().toString())
+    const upFrontCost = blobTx.getUpfrontCost(BigInt(tip.baseFeePerGas[1])) * 2n
+    const maxFee = parseInt(tip.baseFeePerGas[1]) > parseInt(tip.reward[0]) ? parseInt(tip.baseFeePerGas[1]) : parseInt(tip.reward[0])
+    const nonce = await wallet!.provider.request({
+      "method": "eth_getTransactionCount",
+      "params": [wallet?.accounts[0].address ]
     }) as string
-    console.log(res)
-    const { v, r, s } = fromRpcSig(res)
-    
-    const signedTx = tx?.addSignature(v, r, s,true)
+    const tx = FeeMarketEIP1559Transaction.fromTxData({ nonce: parseInt(nonce), to: signedTx.getSenderAddress(), value: upFrontCost, gasLimit: 0x5208, maxFeePerGas: maxFee, maxPriorityFeePerGas: parseInt(tip.reward[0]) })
+    const res = await wallet!.provider.request({
+      "method": "eth_sendTransaction",
+      "params": [{...tx.toJSON(), ...{"from": wallet?.accounts[0].address}} ]
+    })
+    console.log('tx hash', res)
+    let done = false
+    while (!done) {
+      console.log('lets get a transaction receipt')
+      const receipt = await wallet?.provider.request({
+        "method": "eth_getTransactionReceipt",
+        "params": [res]
+      }) as any
+      if (receipt === undefined || receipt === null) {
+        console.log('tx not mined yet')
+        await new Promise((resolve) => setTimeout(resolve, 12000))
+      } else {
+        done = true
+      }
+    }
     console.log(signedTx!.getSenderAddress().toString())
     const res2 = await wallet!.provider.request({
       "method": "eth_sendRawTransaction",
